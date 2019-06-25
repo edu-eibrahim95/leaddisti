34fb6166f273b11fc807c29239000dd69ab923fa
Ibrahim E.Gad <eibrahim95@gmail.com>
2019-06-25 16:21:30 +0200

    mail queue

--- app/Mail/LeadMailable.php
+++ app/Mail/LeadMailable.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Storage;
 
-class LeadMailable extends Mailable
+class LeadMailable extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
